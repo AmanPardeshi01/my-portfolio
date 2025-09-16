210f448fdac3673052642c71c338797b1f9fe4da
AmanPardeshi01 <amanpardeshi540@gmail.com>
2025-09-16 19:59:58 +0530

    Updated Base

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,34 @@
-// vite.config.js
-import { defineConfig } from "vite";
+// // vite.config.js
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+//   // 👇 Add or update this line with your GitHub repo name
+//   base: "/my-portfolio/",
+
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+
+import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
+  base: "/my-portfolio/",
+})
 
-  // 👇 Add or update this line with your GitHub repo name
-  base: "/my-portfolio/", 
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
