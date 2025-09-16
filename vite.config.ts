babe46832adb77ae2779567d9659110fd6d0eadb
AmanPardeshi01 <amanpardeshi540@gmail.com>
2025-09-16 19:51:40 +0530

    Updated Portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -10,11 +11,13 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  // base: "/my-portfolio/",
+
+  // 👇 Add or update this line with your GitHub repo name
+  base: "/my-portfolio/",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
-
